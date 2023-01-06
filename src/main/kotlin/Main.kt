cb88c026c3f4c8db0ab6239268e4c68a48e52ed6
Lantonium <49911858+lantonium@users.noreply.github.com>
2023-01-05 19:50:45 -0500

    Update project Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+    var mensaje = "Hello World!"
+
+    val cumple = "14/09/1976"
+    println(mensaje)
+    println(mensaje)
+    println(mensaje)
+
+    mensaje = "bye!"
+
+    println(mensaje)
+    println(cumple)
+    println(mensaje)
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${mensaje} y ${cumple} listo!")
+}
